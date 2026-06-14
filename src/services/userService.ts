c646c8466080245300bef373cd3e705622640eb0
Dayron Cueva <dayron.cueva@gmail.edu.pe>
2026-06-13 19:19:38 -0500

    subiendo avance

---
+++ src/services/userService.ts
@@ -0,0 +1,28 @@
+import api from './api';
+import {User, Book, Favorite,ReadingHistory, UserRegister,UserLogin} from '../types';
+
+export const userRegister = async (datos:UserRegister) => {
+    const response = await api.post("/api/auth/register",datos);
+    return response.data;
+}
+export const userLogin = async (datos:UserLogin)=> {
+    const response = await api.post("/api/auth/login",datos);
+    return response.data;
+}
+export const getBooks = async ():Promise<Book[]>=> {
+    const response = await api.get<Book[]>("/api/books/list");
+    return response.data;
+}
+// parametro para getFavorites
+export const getFavorites = async (userId:number): Promise <Favorite[]>=>{
+    const response = await api.get<Favorite[]>("api/user/favorites/{userId}")
+    return response.data;
+}
+export const deleteFavorites = async(): => {
+    const response =
+    return response.data;
+}
+export const getHistory = async(userId:number): Promise<> {
+    const response = 
+    return response.data;
+}
